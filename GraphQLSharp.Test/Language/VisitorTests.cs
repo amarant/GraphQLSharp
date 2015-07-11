d0e2d4d9b085a355364d1d5453b4a08daa7e085e
Arnaud Marant <arnaudmarant@gmail.com>
2015-07-11 10:23:48 +0200

    Add output test for OO visitor.

---
+++ GraphQLSharp.Test/Language/VisitorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using GraphQLSharp.Language;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GraphQLSharp.Test.Language
+{
+    public class VisitorTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public VisitorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
+        public class ToStringVisitor : GenericVisitor
+        {
+            private readonly ITestOutputHelper _output;
+
+            public ToStringVisitor(ITestOutputHelper output)
+            {
+                _output = output;
+            }
+
+            public override VisitAction Enter(INode node)
+            {
+                _output.WriteLine("+" + node.GetType().Name);
+                return NoAction;
+            }
+
+            public override VisitAction Leave(INode node)
+            {
+                _output.WriteLine("-" + node.GetType().Name);
+                return NoAction;
+            }
+        }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            var source = new Source("{\n  node(id: 4) {\n    id,\n    name\n  }\n}\n");
+            var result = Parser.Parse(source);
+            var visitor = new ToStringVisitor(_output);
+            result.Accept(visitor);
+        }
+    }
+}
