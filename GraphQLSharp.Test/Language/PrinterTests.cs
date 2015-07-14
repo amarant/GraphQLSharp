bfabc66d8ddd2be0128d407412ca9735d73f9210
Arnaud Marant <arnaudmarant@gmail.com>
2015-07-14 23:48:57 +0200

    Add test for the printer.

---
+++ GraphQLSharp.Test/Language/PrinterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GraphQLSharp.Language;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphQLSharp.Test.Language
+{
+    [TestClass]
+    public class PrinterTests
+    {
+        [TestMethod]
+        public void DoesNotAlterAst()
+        {
+            var kitchenSink = FileUtils.KitchenSink.Value;
+            var ast = Parser.Parse(kitchenSink);
+            var astCopy = Parser.Parse(kitchenSink);
+            var printer = new Printer();
+            var print = printer.VisitDocument(ast);
+            ast.ShouldBeEquivalentToDeepDynamic(astCopy);
+        }
+    }
+}
