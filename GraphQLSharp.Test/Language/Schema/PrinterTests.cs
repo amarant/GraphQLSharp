a469e3dcab13ba4d90d45435c11ad812701d1380
Arnaud Marant <arnaudmarant@gmail.com>
2015-07-30 21:08:52 +0200

    test add schema printer tests structure

---
+++ GraphQLSharp.Test/Language/Schema/PrinterTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace GraphQLSharp.Test.Language.Schema
+{
+    public class PrinterTests
+    {
+        [Fact]
+        public void PrintsMinimalAst()
+        {
+        }
+
+        [Fact]
+        public void ProducesHelpfulErrorMessages()
+        { }
+
+        [Fact]
+        public void PrintsKitchenSink()
+        {
+            
+        }
+    }
+}
